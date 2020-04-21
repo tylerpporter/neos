19b0ef63d26ecdb8b3bae5c58584d515aea97456
Tyler Porter <tylerpporter@gmail.com>
2020-04-20 19:57:23 -0600

    Refactor near_earth_objects.rb and start.rb

--- near_earth_objects.rb
+++ near_earth_objects.rb
@@ -1,48 +1,50 @@
 require 'faraday'
 require 'figaro'
 require 'pry'
-# Load ENV vars via Figaro
+
 Figaro.application = Figaro::Application.new(environment: 'production', path: File.expand_path('../config/application.yml', __FILE__))
 Figaro.load
 
 class NearEarthObjects
+
   def self.find_neos_by_date(date)
+    @@date ||= date
     {
-      astroid_list: formatted_asteroid_data(date),
-      biggest_astroid: largest_astroid_diameter(date),
-      total_number_of_astroids: total_number_of_astroids(date)
+      astroid_list: formatted_asteroid_data,
+      biggest_astroid: largest_astroid_diameter,
+      total_number_of_astroids: total_number_of_astroids
     }
   end
 
-private
+  private
 
-  def self.conn(date)
+  def self.conn
     Faraday.new(
       url: 'https://api.nasa.gov',
-      params: { start_date: date, api_key: ENV['nasa_api_key']}
+      params: { start_date: @@date, api_key: ENV['nasa_api_key']}
     )
   end
 
-  def self.asteroids_list_data(date)
-    conn(date).get('/neo/rest/v1/feed')
+  def self.asteroids_list_data
+    conn.get('/neo/rest/v1/feed')
   end
 
-  def self.parsed_asteroids_data(date)
-    JSON.parse(asteroids_list_data(date).body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
+  def self.parsed_asteroids_data
+    JSON.parse(asteroids_list_data.body, symbolize_names: true)[:near_earth_objects][:"#{@@date}"]
   end
 
-  def self.largest_astroid_diameter(date)
-    parsed_asteroids_data(date).map do |astroid|
+  def self.largest_astroid_diameter
+    parsed_asteroids_data.map do |astroid|
       astroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i
     end.max { |a,b| a<=> b}
   end
 
-  def self.total_number_of_astroids(date)
-    parsed_asteroids_data(date).count
+  def self.total_number_of_astroids
+    parsed_asteroids_data.count
   end
 
-  def self.formatted_asteroid_data(date)
-    parsed_asteroids_data(date).map do |astroid|
+  def self.formatted_asteroid_data
+    parsed_asteroids_data.map do |astroid|
       {
         name: astroid[:name],
         diameter: "#{astroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i} ft",
@@ -50,5 +52,5 @@ private
       }
     end
   end
-  
+
 end
--- start.rb
+++ start.rb
@@ -1,41 +1,75 @@
 require_relative 'near_earth_objects'
 
-puts "________________________________________________________________________________________________________________________________"
-puts "Welcome to NEO. Here you will find information about how many meteors, astroids, comets pass by the earth every day. \nEnter a date below to get a list of the objects that have passed by the earth on that day."
-puts "Please enter a date in the following format YYYY-MM-DD."
-print ">>"
+class NearEarthObjectsDisplay < NearEarthObjects
 
-date = gets.chomp
-astroid_details = NearEarthObjects.find_neos_by_date(date)
-astroid_list = astroid_details[:astroid_list]
-total_number_of_astroids = astroid_details[:total_number_of_astroids]
-largest_astroid = astroid_details[:biggest_astroid]
+  def self.start
+    intro_dashboard
+    @@date ||= gets.chomp
+    @@asteroid_info ||= NearEarthObjects.find_neos_by_date(@@date)
+    return_dashboard
+  end
 
-column_labels = { name: "Name", diameter: "Diameter", miss_distance: "Missed The Earth By:" }
-column_data = column_labels.each_with_object({}) do |(col, label), hash|
-  hash[col] = {
-    label: label,
-    width: [astroid_list.map { |astroid| astroid[col].size }.max, label.size].max}
+  private
+
+  def self.intro_dashboard
+    puts "________________________________________________________________________________________________________________________________"
+    puts "Welcome to NEO. Here you will find information about how many meteors, astroids, comets pass by the earth every day. \nEnter a date below to get a list of the objects that have passed by the earth on that day."
+    puts "Please enter a date in the following format YYYY-MM-DD."
+    print ">>"
+  end
+
+  def self.column_labels
+    { name: "Name", diameter: "Diameter", miss_distance: "Missed The Earth By:" }
+  end
+
+  def self.column_data
+    column_labels.each_with_object({}) do |(col, label), hash|
+      hash[col] = {
+        label: label,
+        width: [@@asteroid_info[:astroid_list].map { |astroid| astroid[col].size }.max, label.size].max}
+    end
+  end
+
+  def self.header
+    "| #{ column_data.map { |_,col| col[:label].ljust(col[:width]) }.join(' | ') } |"
+  end
+
+  def self.divider
+    "+-#{column_data.map { |_,col| "-"*col[:width] }.join('-+-') }-+"
+  end
+
+  def self.format_row_data(row_data, column_info)
+    row = row_data.keys.map { |key| row_data[key].ljust(column_info[key][:width]) }.join(' | ')
+    puts "| #{row} |"
+  end
+
+  def self.create_rows(astroid_data, column_info)
+    astroid_data.each { |astroid| format_row_data(astroid, column_info) }
+  end
+
+  def self.formated_date
+    DateTime.parse(@@date).strftime("%A %b %d, %Y")
+  end
+
+  def self.return_dashboard_text
+    puts "______________________________________________________________________________"
+    puts "On #{formated_date}, there were #{@@asteroid_info[:total_number_of_astroids]} objects that almost collided with the earth."
+    puts "The largest of these was #{@@asteroid_info[:biggest_astroid]} ft. in diameter."
+    puts "\nHere is a list of objects with details:"
+  end
+
+  def self.return_dashboard_table
+    puts divider
+    puts header
+    create_rows(@@asteroid_info[:astroid_list], column_data)
+    puts divider
+  end
+
+  def self.return_dashboard
+    return_dashboard_text
+    return_dashboard_table
+  end
+
 end
 
-header = "| #{ column_data.map { |_,col| col[:label].ljust(col[:width]) }.join(' | ') } |"
-divider = "+-#{column_data.map { |_,col| "-"*col[:width] }.join('-+-') }-+"
-
-def format_row_data(row_data, column_info)
-  row = row_data.keys.map { |key| row_data[key].ljust(column_info[key][:width]) }.join(' | ')
-  puts "| #{row} |"
-end
-
-def create_rows(astroid_data, column_info)
-  rows = astroid_data.each { |astroid| format_row_data(astroid, column_info) }
-end
-
-formated_date = DateTime.parse(date).strftime("%A %b %d, %Y")
-puts "______________________________________________________________________________"
-puts "On #{formated_date}, there were #{total_number_of_astroids} objects that almost collided with the earth."
-puts "The largest of these was #{largest_astroid} ft. in diameter."
-puts "\nHere is a list of objects with details:"
-puts divider
-puts header
-create_rows(astroid_list, column_data)
-puts divider
+NearEarthObjectsDisplay.start
